24d921e37ef079af4e40639508271b16d506e85e
Garima50 <garima1824.be23@chitkara.edu.in>
2024-05-15 17:33:51 +0530

    Adding Version 3

---
+++ ON-SCREEN-KEYBOARD V3/vkjs.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  $("#inputbox").focus();
+
+  $('#keyboardBase').css({
+      opacity: '1'
+  });
+
+  // Handling key press
+  $('#keyboardBase').on('click', '.key.inkey', function () {
+      let text = $(this).text();
+      if ($(this).hasClass('space')) {
+          text = ' ';
+      }
+      $("#inputbox").val($("#inputbox").val() + text).focus();
+  });
+
+  // Handling click events for the CapsLock key to toggle its state
+  let capsLockActive = false;
+  $('#keyboardBase').on('click', '.key.capslock', function () {
+  capsLockActive = !capsLockActive;
+  $(this).toggleClass('active', capsLockActive);
+    // Convert letters to uppercase if CapsLock is active
+  $('.key.inkey').each(function () {
+      let letter = $(this).text();
+      if (capsLockActive && letter >= 'a' && letter <= 'z') {
+      $(this).text(letter.toUpperCase());
+      } else {
+      $(this).text(letter.toLowerCase());
+      }
+  });
+  });
+
+  // Handling Shift
+  let shiftActive = false;
+  $('#keyboardBase').on('mousedown', '.key.shift', function (e) {
+      e.preventDefault();
+      shiftActive = !shiftActive;
+      $(this).toggleClass('active', shiftActive);
+      // Toggle uppercase if Shift is active, otherwise toggle based on Caps Lock
+      $('#keyboardBase').toggleClass('uppercase', shiftActive || capsLockActive);
+  });
+
+  // Handling Backspace
+  $('#keyboardBase').on('click', '.key.delete', function () {
+      let inputVal = $("#inputbox").val();
+      $("#inputbox").val(inputVal.slice(0, -1)).focus();
+  });
+
+  // Handling Enter
+  $('#keyboardBase').on('click', '.key.return', function () {
+      $("#inputbox").val($("#inputbox").val() + "\n").focus();
+  });
+
+  // Highlighting pressed key
+  $('#keyboardBase').on('keydown', function (event) {
+      event.preventDefault();
+      let keyCode = event.keyCode;
+      let keyElement = $('[data="' + keyCode + '"]');
+      keyElement.addClass('highlight');
+      setTimeout(function () {
+          keyElement.removeClass('highlight');
+      }, 1000);
+  });
+
+  // Resetting key styles on blur
+  $('#keyboardBase').on('blur', function () {
+      $('.key').removeClass('highlight');
+  });
+});
